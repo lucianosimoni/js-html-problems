a729c12adcd2dc0fd8da9037b9f1985b2312124e
OnekaAustin <neferonyx@gmail.com>
2022-10-17 15:46:45 +0100

    JS Problems

---
+++ timetables/main.js
@@ -0,0 +1,15 @@
+// Find all the multiplication tables from 1 through 12, up to 12. For example:
+
+// 7 times table
+// x1	x2	x3	x4	x5	x6	x7	x8	x9	x10	x11	x12
+// 7	14	21	28	35	42	49	56	63	70	77	84
+
+Input: 12 times table
+Output:To give the numbers from x1 up to x12 timetable
+
+function multiplication (number) {
+    for (let i = 1; i < 12; i++) {
+
+    }
+
+}
